68f0e88bc4cf2a78d4fd433c52311faa3e12876a
Asher Leffler <asher.leffler@gmail.com>
2024-08-21 15:22:25 -0400

    refactor: Improve ExtractPage component by using try-catch block for adding money elements

--- src/components/ExtractPage.jsx
+++ src/components/ExtractPage.jsx
@@ -13,27 +13,30 @@ const ExtractPage = ({ balance, setBalance, max, min }) => {
   }
   useEffect(() => {
     if (randomMoney !== 0) {
-      const addedMoney = document.createElement("p");
-      const currency = document.createElement('i');
-      const container = document.createElement('div');
-      addedMoney.textContent = randomMoney.toFixed(2);
-      currency.classList.add('fa-solid', 'fa-coins');
-      addedMoney.classList.add("extractedMoney");
-      container.classList.add('extractedMoneyCont');
-      container.style.left = `${Math.random() * (62 - 32) + 32}%`;
-      container.style.top = `${Math.random() * (34 - 24) + 24}%`;
-      document.querySelector(".extractPage").appendChild(container);
-      container.appendChild(currency);
-      container.appendChild(addedMoney);
-      container.classList.add("display");
-      setTimeout(() => {
-        document.querySelector(".extractedMoneyCont").classList.remove("display");
+      try {
+        const addedMoney = document.createElement("p");
+        const currency = document.createElement('i');
+        const container = document.createElement('div');
+        addedMoney.textContent = randomMoney.toFixed(2);
+        currency.classList.add('fa-solid', 'fa-coins');
+        addedMoney.classList.add("extractedMoney");
+        container.classList.add('extractedMoneyCont');
+        container.style.left = `${Math.random() * (62 - 32) + 32}%`;
+        container.style.top = `${Math.random() * (34 - 24) + 24}%`;
+        document.querySelector(".extractPage").appendChild(container);
+        container.appendChild(currency);
+        container.appendChild(addedMoney);
+        container.classList.add("display");
         setTimeout(() => {
-          document
-            .querySelector(".extractPage")
-            .removeChild(document.querySelector(".extractedMoneyCont"));
-        }, 150);
-      }, 1000);
+          document.querySelector(".extractedMoneyCont").classList.remove("display");
+          setTimeout(() => {
+            document
+              .querySelector(".extractPage")
+              .removeChild(document.querySelector(".extractedMoneyCont"));
+          }, 150);
+        }, 1000);
+      } catch (error) {
+      }
     }
   }, [randomMoney]);
   return (
