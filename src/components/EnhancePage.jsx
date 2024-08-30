847305147441a9866fb21ef04d83136b692200e1
Asher Leffler <asher.leffler@gmail.com>
2024-08-30 10:10:06 -0400

    Update EnhancePage.jsx

--- src/components/EnhancePage.jsx
+++ src/components/EnhancePage.jsx
@@ -322,7 +322,7 @@ const EnhancePage = ({
       };
     }
   }, [currentPage]);
-  const [time, setTime] = useState(80);
+  const [time, setTime] = useState(100);
   useEffect(() => {
     if (currentPage === "beamEnhance") {
       const imgCont = document.querySelector(".imgCont");
